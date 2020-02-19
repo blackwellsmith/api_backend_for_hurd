9619ccebbeb48d227a58d88565bdf4c28035ea39
blackwellsmith <blackwellsmithiv@gmail.com>
2020-02-19 15:09:22 -0500

    added get_current_user method in sessions controller for get request

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
 
     def create
-       @user = User.find_by(email: params[:sessions][:email])
+       @user = User.find_by(email: params[:session][:email])
 
-       if @user && @user.authenticate(params[:session][:email])
+       if @user && @user.authenticate(params[:session][:password])
          session[:user_id] = @user.id
          render json: @user
        else
@@ -11,7 +11,16 @@ class SessionsController < ApplicationController
              error: "Invalid Credentials"
          }
         end
+    end
 
+    def get_current_user
+        if logged_in?
+            render json: current_user
+        else
+            render json: {
+                error: 'No one logged in'
+            }
+        end
     end
 
 
