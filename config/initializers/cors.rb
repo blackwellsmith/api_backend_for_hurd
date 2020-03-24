5f8806ad8d4d3feca5169dc8de033f1da87b089b
blackwellsmith <blackwellsmithiv@gmail.com>
2020-03-24 18:26:19 -0400

    replaced credentials true wit expose authorization

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,6 +12,6 @@
     resource '*',
        headers: :any,
        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-       credentials: true
+       expose: ['Authorization']
    end
  end
