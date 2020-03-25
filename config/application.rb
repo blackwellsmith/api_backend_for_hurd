d05e31492613050979a1f4ef0f72b4eae88f6495
blackwellsmith <blackwellsmithiv@gmail.com>
2020-03-25 16:55:20 -0400

    moved samesite secure to line 34

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module ApiBackendForHurd
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore, key: '_cookie_name', same_site: 'Secure'
+    config.middleware.use ActionDispatch::Cookies, same_site: 'Secure'
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_cookie_name'
   end
 end
