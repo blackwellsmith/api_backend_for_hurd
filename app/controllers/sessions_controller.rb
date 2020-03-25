f89c728dace014b81b04f5116a0f219f1dd1b171
blackwellsmith <blackwellsmithiv@gmail.com>
2020-03-25 18:36:03 -0400

    adding response header in sessions create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
        if @user && @user.authenticate(params[:session][:password])
          session[:user_id] = @user.id
-
+         response.headers['Set-Cookie'] = 'Secure;SameSite=None'
          render json:  @user
             
        else
